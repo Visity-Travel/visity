fc530d9408548f17e3bccc9d566a89b63d61e955
bennbaia <mh665072@dal.ca>
2020-05-01 03:27:19 -0300

    base for the search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as mt from '@material-ui/core'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1 className="App-logo">VISITY</h1>
-      </header>
+    <div class="App">
+      <h1>VISITY</h1>
+      <div style={{
+        display: "flex",
+        justifyContent: "center"
+      }}>
+        <mt.Card style={{ width: "70%", display: "flex", justifyContent: "center" }}>
+          <mt.CardContent style={{ display: "flex", justifyContent: "center", width: "100%"}}>
+            <mt.Typography variant="h6"></mt.Typography>
+            <mt.TextField placeholder="enter location" style={{width:"100%"}}></mt.TextField>
+          </mt.CardContent>
+          <mt.CardActions><mt.Button>search</mt.Button></mt.CardActions>
+        </mt.Card>
+      </div>
     </div>
   );
 }
