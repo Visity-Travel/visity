2c8c11c254652178448208c90078dca0f0e03a8c
bennbaia <mh665072@dal.ca>
2020-05-01 05:59:20 -0300

    added scrollable scalable stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
+import './App.css'
+import React from 'react'
 import * as mt from '@material-ui/core'
-import './App.css';
+import Landing from "./Components/Landing.js"
 
 function App() {
   return (
-    <div class="App">
-      <mt.Typography variant="h2">Visity</mt.Typography>
-      <div style={{
-        display: "flex",
-        justifyContent: "center"
-      }}>
-        <mt.Card style={{ width: "70%", display: "flex", justifyContent: "center" }}>
-          <mt.CardContent style={{ display: "flex", justifyContent: "center", width: "100%"}}>
-            <mt.TextField placeholder="enter location" style={{width:"100%"}}></mt.TextField>
-          </mt.CardContent>
-          <mt.CardActions><mt.Button>search</mt.Button></mt.CardActions>
-        </mt.Card>
-      </div>
-    </div>
+    <Landing />
   );
 }
-
 export default App;
---
+++ src/Components/Landing.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import * as mt from '@material-ui/core'
+import '../App.css'
+
+export default class Landing extends Component {
+    state = {
+        categories: ["Food", "Nature", "Language", "City", "Museum"]
+    }
+
+    render() {
+        return (
+            <div class="App" style={{ marginTop: "1%" }}>
+                <mt.Typography variant="h2">Visity</mt.Typography>
+                <div style={{
+                    display: "flex",
+                    justifyContent: "center"
+                }}>
+                    <mt.Card style={{ width: "84%", display: "flex", justifyContent: "center" }}>
+                        <mt.CardContent style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+                            <mt.TextField placeholder="Where do you want to go?" style={{ width: "100%" }}></mt.TextField>
+                        </mt.CardContent>
+                        <mt.CardActions><mt.Button>search</mt.Button></mt.CardActions>
+                    </mt.Card>
+                </div>
+
+                <mt.Typography variant="h4" style={{ marginTop: "7%", marginBottom: "2%" }}>Catergories</mt.Typography>
+                <mt.AppBar color="transparent" position="static" >
+                    <mt.Tabs
+                    variant="scrollable"
+                    >
+                        {this.state.categories.map(
+                            (category) =>
+                                <mt.Card style={{ minWidth: "390px", marginRight: "2%" }}>
+                                    <mt.CardHeader
+                                        title={category + " Tours"}
+                                        subheader="prices between $50-$80"
+                                    />
+                                    <mt.CardMedia
+                                        style={{ height: "10em" }}
+                                        image={require("../Images/" + category + ".jpg")}
+                                    />
+                                    <mt.CardActions style={{ justifyContent: "center" }}>
+                                        <mt.Button style={{ width: "70%" }}>View {category} Tours</mt.Button>
+                                    </mt.CardActions>
+                                </mt.Card>
+                        )}
+                    </mt.Tabs>
+                </mt.AppBar>
+
+                <mt.Typography variant="h4" style={{ marginTop: "7%", marginBottom: "2%" }}>Top Destinations</mt.Typography>
+                <mt.AppBar color="transparent" position="static" >
+                    <mt.Tabs
+                    variant="scrollable"
+                    >
+                        {this.state.categories.map(
+                            (category) =>
+                                <mt.Card style={{ minWidth: "390px", marginRight: "2%" }}>
+                                    <mt.CardHeader
+                                        title={category + " Tours"}
+                                        subheader="prices between $50-$80"
+                                    />
+                                    <mt.CardMedia
+                                        style={{ height: "10em" }}
+                                        image={require("../Images/" + category + ".jpg")}
+                                    />
+                                    <mt.CardActions style={{ justifyContent: "center" }}>
+                                        <mt.Button style={{ width: "70%" }}>View {category} Tours</mt.Button>
+                                    </mt.CardActions>
+                                </mt.Card>
+                        )}
+                    </mt.Tabs>
+                </mt.AppBar>
+
+                <mt.Typography variant="h4" style={{ marginTop: "7%", marginBottom: "2%" }}>Top Tours</mt.Typography>
+                <mt.AppBar color="transparent" position="static" >
+                    <mt.Tabs
+                    variant="scrollable"
+                    >
+                        {this.state.categories.map(
+                            (category) =>
+                                <mt.Card style={{ minWidth: "390px", marginRight: "2%" }}>
+                                    <mt.CardHeader
+                                        title={category + " Tours"}
+                                        subheader="prices between $50-$80"
+                                    />
+                                    <mt.CardMedia
+                                        style={{ height: "10em" }}
+                                        image={require("../Images/" + category + ".jpg")}
+                                    />
+                                    <mt.CardActions style={{ justifyContent: "center" }}>
+                                        <mt.Button style={{ width: "70%" }}>View {category} Tours</mt.Button>
+                                    </mt.CardActions>
+                                </mt.Card>
+                        )}
+                    </mt.Tabs>
+                </mt.AppBar>
+                
+            </div>
+        );
+    }
+}
