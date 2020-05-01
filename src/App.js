3bd67753c274fabf082d5c613677e20017435eef
bennbaia <mh665072@dal.ca>
2020-05-01 03:32:37 -0300

    messing with the design a bit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import './App.css';
 function App() {
   return (
     <div class="App">
-      <h1>VISITY</h1>
+      <mt.Typography variant="h2">Visity</mt.Typography>
       <div style={{
         display: "flex",
         justifyContent: "center"
       }}>
         <mt.Card style={{ width: "70%", display: "flex", justifyContent: "center" }}>
           <mt.CardContent style={{ display: "flex", justifyContent: "center", width: "100%"}}>
-            <mt.Typography variant="h6"></mt.Typography>
             <mt.TextField placeholder="enter location" style={{width:"100%"}}></mt.TextField>
           </mt.CardContent>
           <mt.CardActions><mt.Button>search</mt.Button></mt.CardActions>
